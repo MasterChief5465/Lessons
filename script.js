d4aea7477dc981cddf4d34822e80c678b73b4a9d
Octavian <octaw13@gmail.com>
2020-04-05 19:27:25 +0300

    Lesson2

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-let money = prompt("Ваш бюджет на месяц?", ""),
+let money = +prompt("Ваш бюджет на месяц?", ""),
 	time = prompt("Введите дату в формате YYYY-MM-DD", "");
 
 let appData = {
@@ -12,12 +12,26 @@ let appData = {
 	savings: false
 };
 
-let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
-	b = prompt("Во сколько обойдется?", ''),
-	c = prompt("Введите обязательную статью расходов в этом месяце", ''),
-	d = prompt("Во сколько обойдется?", '');
+for (let i = 0; i < 2; i++) {
+	let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+		b = prompt("Во сколько обойдется?", '');
+	if ((typeof (a)) === 'string' && (typeof (a)) != null && (typeof (b)) === 'string' && (typeof (b)) != null &&
+		a != "" && b != "" && a.length < 50) {
+		console.log("done");
+		appData.expenses[a] = b;
+	} else { }
+}
 
-appData.expenses[a] = b;
-appData.expenses[c] = d;
+appData.moneyPerDay = appData.budget / 30;
 
-alert(appData.budget / 30);
+alert("Daily budget= " + appData.moneyPerDay);
+
+if (appData.moneyPerDay < 100) {
+	console.log("Minim");
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+	console.log("Medium");
+} else if (appData.moneyPerDay > 2000) {
+	console.log("Sufficient");
+} else {
+	console.log("error");
+}
